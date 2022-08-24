4c2cc42a22183d498e90ef28b11fe03385907bec
Timo <tpontine@hive.fi>
2022-08-24 09:46:39 +0300

    added README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>ft_printf project notes</h1>
+<code>ft_float_to_ascii
+<ul>
+	<li>check how this works at limits</li>
+	<li>rounding handling</li>
+</ul>
+
+
+</code>
--- func_pointers.c
+++ func_pointers.c
@@ -41,10 +41,11 @@ void ft_print_str(va_list data)
 
 char	*initialize_sequence(int size, int addition) //size + add bytes
 {
-
 	int		i;
 	char	*sequence;
 
+	if (size < 0)
+		size *= -1;
 	if (addition < 0)
 		addition *= -1;
 	size += addition;
@@ -58,7 +59,6 @@ char	*initialize_sequence(int size, int addition) //size + add bytes
 		return (sequence);
 	}
 	return (0);
-	
 }
 
 float	to_pwr(int exp, int base)
@@ -88,33 +88,54 @@ float	to_pwr(int exp, int base)
 void	adder(char **addto, char *num)
 {
 	int	i;
-	int c;
 
 	i = 0;
-	c = 0;
-	while (*(*addto + i) && *(num + c))
-	{
+	while (*(num + i) && *((*addto + i)))
 		i++;
-		c++;
-	}
-		
-	while (*(*addto + i) != '.' || *(num + c) != '.')
+	i--;
+
+	// while (*(*addto + i) != '.' && *(num + i) != '.')
+	while (i > 1)
 	{
-		if (*(*addto + i) + (*(num + c) - 48) > '9')
+		if (*(*addto + i) + (*(num + i) - 48) > '9')
 		{
 			*(*addto + i - 1) += 1;
-			*(*addto + i) += *(num + c) - 10 - 48;	
+			*(*addto + i) += *(num + i) - 10 - 48;	
 		}
 		else
-			*(*addto + i) += *(num + c) - 48;	
+			*(*addto + i) += *(num + i) - 48;	
 		i--;
-		c--;
 	}
 }
 
-void	multiplyer(char **s, int multiplyer)
+void	multiplyer(char **s, int multiplyer, int len)
 {
+	int		i;
+	char	memo = 48;
+	char	digit;
+	char	next = 0;
 
+	i = len - 1;
+	while (i >= 0)
+	{
+		digit = *(*s + i) - 48;
+		digit *= multiplyer;
+		digit += next;
+		next = 0;
+		if (digit > 9)
+		{
+			next = digit / 10;
+			digit %= 10;
+			digit += 48;
+			*(*s + i) = digit;
+		}
+		else
+		{
+			digit += 48;
+			*(*s + i) = digit;
+		}
+		i--;
+	}
 }
 
 char *count_fraction(int exp)
@@ -126,11 +147,10 @@ char *count_fraction(int exp)
 
 	if (exp == 0)
 	{
-		printf("1.0");
+		// printf("1.0");
 		return ("1.0");
 	}
 		
-
 	if (exp < 0)
 		exp *= -1;
 
@@ -138,37 +158,13 @@ char *count_fraction(int exp)
 
 	seq = initialize_sequence(exp,2);
 	seq[exp+1] = '1';
-	
-	char memo = 48;
-	char digit;
-	char next = 0;
 	while (c < exp)
 	{
-		i = exp + 1;
-		while (i >= 0)
-		{
-			digit = *(seq + i) - 48;
-			digit *= 5;
-			digit += next;
-			next = 0;
-			if (digit > 9)
-			{
-				next = digit / 10;
-				digit %= 10;
-				digit += 48;
-				*(seq + i) = digit;
-			}
-			else
-			{
-				digit += 48;
-				*(seq + i) = digit;
-			}
-			i--;
-		}
+		multiplyer(&seq, 5, exp+2);
 		c++;
 	}
 	seq[1] = '.';
-	printf("%s", seq);
+	// printf("%s\n", seq);
 	return (seq);
 }
 
@@ -191,17 +187,21 @@ int		ret_exp(float f)
 	return (exp);
 }
 
-void	count_integer(int exp)
+char	*count_integer(int exp)
 {
 	int		i;
 	char	*result;
 
 	result = initialize_sequence(40,0);
 	result[39] = '1';
-	i = 0;
-	
-	while (i < exp)
 
+	i = 0;
+	while (i < exp)
+	{
+		multiplyer(&result,2,40);
+		i++;
+	}
+	return (result);
 }
 
 void	ft_float_to_ascii(float f)
@@ -209,38 +209,84 @@ void	ft_float_to_ascii(float f)
 	int	*p;
 	int	i;
 	int	c;
-	int sign;
 	int	exp;
 	char *manchar;
-	char *result;
+	char *result_frac;
+	char *result_int;
 	
 	exp = ret_exp(f);
-	result = initialize_sequence(1000,0);
+	result_frac = initialize_sequence(exp - 23,3);
+	result_int = initialize_sequence(40,1);
+	result_frac[1] = '.';
 
 	p = (int *)&f;
-	i = 22;
-	c = 1;
-	printf("hidden bit value:\t");
-	count_fraction(exp);
-	while (i >= 0)
+	i = 0;
+	c = 23;
+	
+	while (i < 23)
 	{
-		printf("\nbit %d value:\t\t",i+1);
 		if((*p >> i) & 1)
 		{
 			if (exp - c < 0)
+			{
+				// printf("counting with:%d\n",exp-c);
 				manchar = count_fraction(exp - c);
+				adder(&result_frac,manchar);
+				// printf("adding bit%d:\t%s\n", i+1, manchar);
+				// printf("result:\t\t%s\n", result_frac);
+			}
 			else
+			{
+				// printf("counting with:%d\n",exp-c);
 				manchar = count_integer(exp - c);
-			// adder(&result,manchar);
-			// free(manchar);
-		}
-		c++;	
-		i--;
+				adder(&result_int,manchar);
+				// printf("adding bit%d:\t%s\n", i+1, manchar);
+				// printf("result:\t\t%s\n", result_int);
+			}
+			ft_bzero(manchar, ft_strlen(manchar));
+			ft_strdel(&manchar);
+		}	
+		c--;	
+		i++;
 	}
-	i = 23;
-	sign = 1;
+	printf("%d\n",exp);
+	if (exp != -126)
+	{
+		// printf("counting hidden bit:");
+		if (exp < 0)
+		{	
+			manchar = count_fraction(exp);
+			adder(&result_frac,manchar);
+			// printf("adding bit:\t%s\n", manchar);
+			// printf("result:\t\t%s\n", result_frac);
+		}
+		else
+		{
+			manchar = count_integer(exp);
+			adder(&result_int,manchar);
+			// printf("adding bit:\t%s\n", manchar);
+			// printf("result:\t\t%s\n", result_int);
+		}
+			
+	}
+	
 	if (((*p >> 31) & 1))
-		sign = -1;
+		ft_putchar('-');
+
+	i = 0;
+	while (result_int[i] == '0')
+		i++;
+	if (i != 40)
+		ft_putstr(result_int+i);
+	else
+		ft_putchar('0');
+	i = 0;
+	c = 1;
+	while (*(result_frac + i))
+		i++;
+	while (c < i)
+		ft_putchar(*(result_frac + c++));
+	
 }
 
 // This is a function that takes a function and int as parameters.
@@ -294,14 +340,19 @@ void	ft_printf(const char *str, ...)
 
 int main()
 {
-	float f = 0.1;
-	// float f = 1.17549435082e-38;
-	// float f = 0.15625;
+	// float f = 104857.601562;
+	// float f = 1.17549435082e-38; //ei toimi
+	// float f = 0.1;
+	// float f = 1.0e38; //lähellä maksia???
+	// float f = 1.5e-37; //tämä on viimeinen joka toimii.... -38 ei toimi enää
+	float f = -1.0e-37;
 
+	printf("%.146f\n",f);
 	ft_float_to_ascii(f);
-	// printf("%.126f\n",f);
 
-	count_fraction(0);
+
+	// printf("%s",count_integer(0));
+	// printf("%s",count_fraction(-2));
 
 	return (0);
 }
