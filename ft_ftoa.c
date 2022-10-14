7adcf04b28f2ebef08ac15f967f14103d4bfe863
Timo <tpontine@hive.fi>
2022-10-14 10:46:39 +0300

    backup

--- ft_ftoa.c
+++ ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: tpontine <tpontine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 10:02:54 by tpontine          #+#    #+#             */
-/*   Updated: 2022/10/14 08:36:33 by tpontine         ###   ########.fr       */
+/*   Updated: 2022/10/14 10:44:01 by tpontine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -161,35 +161,28 @@ char	*count_integer(int exp)
 	return (result);
 }
 
-// [0][.][0][\0]
-/*
-result_frac = "0.25";
-len_frac = 4;
-m = "0.125";
-new = [][][][][][];
-new = [0][.][2][5][0][\0];
-new = [0][.][3][7][5][\0];
-*/
-
-char	*handle_fraction(int exp, char *result_frac)
+char	*handle_fraction(int exp, char **result_frac)
 {
 	size_t	len_f;
 	size_t	len_m;
+	char	*temp;
 	char	*new;
 	char	*m;
 	char	*addition;
 	
+	temp = ft_strdup(*result_frac);
+	ft_strdel(result_frac);
 	m = count_fraction(exp);
-	len_f = ft_strlen(result_frac);
+	len_f = ft_strlen(temp);
 	len_m = ft_strlen(m);
 	new = (char*)malloc(sizeof(char) * (len_m + 2));
 	addition = (char*)malloc(sizeof(char) * (len_m - len_f + 1));
 	ft_memset(addition, '0', (size_t)(len_m - len_f));
-	new = ft_strcat(result_frac, addition);
+	new = ft_strcat(temp, addition);
 	exp = 0;
 	adder(&new, m);
-	printf("output value: %s\n", new);
 	ft_strdel(&m);
+	ft_strdel(&temp);
 	return (new);
 }
 
@@ -213,38 +206,25 @@ void	ft_ftoa(float f, int precision)
 	
 	exp = ret_exp(f);
 	result_frac = (char *)malloc(sizeof(char) * 4);
-	result_frac[0] = '0';
-	result_frac[1] = '.';
-	result_frac[2] = '0';
+	result_frac = ft_strcpy(result_frac, "0.0");
 	result_frac[3] = '\0';
 	result_int = malloc_and_setzero(40);
 
 	p = (int *)&f;
 	i = 22;
-	printf("exponent: %d\n", exp);
-	printf("result_frac: %s\n",result_frac);
 	if (exp < 0 && exp != -126)
-		handle_fraction(exp, result_frac);
+		handle_fraction(exp, &result_frac);
 	else
 		handle_integer(exp, &result_int);
 	exp--;
-	printf("result_frac: %s\n",result_frac);
 	while (i >= 0)
 	{
 		if((*p >> i) & 1)
 		{
 			if (exp < 0)
-			{
-				printf("exponent: %d\n", exp);
-				handle_fraction(exp, result_frac);
-				printf("result:\t\t%s\n", result_frac);
-			}
+				handle_fraction(exp, &result_frac);
 			else
-			{
-				printf("exponent: %d\n", exp);
 				handle_integer(exp, &result_int);
-				printf("result:\t\t%s\n", result_int);
-			}
 		}	
 		exp--;	
 		i--;
@@ -258,103 +238,22 @@ void	ft_ftoa(float f, int precision)
 		ft_putchar(*(result_int + a++));
 	i = 0;
 	c = 1;
-	// while (*(result_frac + i))
-	// 	i++;
-	while (c < (int)ft_strlen(result_frac) && c <= precision)
+	while (c < (int)ft_strlen(result_frac) && c <= precision + 1)
 		ft_putchar(*(result_frac + c++));
 	
 }
 
 int	main()
-{
-	//TESTAA TÄMÄ KOULULLA, ETTÄ TULEE OIKEA LOPPUTULOS
-	// char	*fraction;
-	// char	*total = malloc_and_setzero(151);
-	// int	result_int = 0;
-	// total[1] = '.';
-	
+{	
 	// float f = 123.23;
 	// float f = 0.1;
-	// float f = 0.1123123123;
+	float f = 0.1123123123;
 	// float f = -0.1123123123;
 	// float f = __FLT_MIN__; //This does not work
 	// float f = __FLT_MAX__;
-
-	// for (int i = 0; i <= 149; i++)
-	// {
-	// 	fraction = count_fraction(i);
-	// 	// printf("exp -%d: %s\n",i,fraction);
-	// }
-
-	// char *first = count_fraction(-17);
-	// char *sec = count_fraction(-18);
-
-	// printf("%s\n", first);
-	// printf("%s\n", sec);
-
-	// printf("%s\n", total);
-	// adder(&total, first);
-	// printf("%s\n", total);
-	// adder(&total, sec);
-	// printf("%s\n", total);
-
-	// for (int i = 8; i >= 5; i--)
-	// {
-	// 	printf("exp: %d result: %d\n",i,to_pwr(i,2));
-	// 	result_int += to_pwr(i,2);
-	// 	printf("%d\n", result_int);
-	// }
-
-	// printf("%d", to_pwr(8,2));
-
-	// ft_ftoa(f,12);
-
-	char *result_frac;
-	// char *m;
 	
-	result_frac = (char *)malloc(sizeof(char) * 4);
-	result_frac[0] = '0';
-	result_frac[1] = '.';
-	result_frac[2] = '0';
-	result_frac[3] = '\0';
-
-	// for (int i = 0; i > -12; i--)
-	// {
-	// 	// result_frac = handle_fraction(i, result_frac);
-	// 	m = count_fraction(i);
-	// 	printf("result: %s\n", m);
-	// }
-	
-	// printf("result: %s\n", s);
-	// printf("manchar:%s\n", m);
-	// adder(&s,m);
-	// printf("result: %s\n", s);
-
-	printf("fraction in the start: %s\n", result_frac);
-	result_frac = handle_fraction(-4, result_frac);
-	result_frac = handle_fraction(-5, result_frac);
-	result_frac = handle_fraction(-8, result_frac);
-	result_frac = handle_fraction(-9, result_frac);
-	result_frac = handle_fraction(-12, result_frac);
-	result_frac = handle_fraction(-13, result_frac);
-	result_frac = handle_fraction(-16, result_frac);
-	result_frac = handle_fraction(-17, result_frac);
-	result_frac = handle_fraction(-20, result_frac);
-	result_frac = handle_fraction(-21, result_frac);
-	result_frac = handle_fraction(-24, result_frac);
-	result_frac = handle_fraction(-25, result_frac);
-	result_frac = handle_fraction(-27, result_frac);
+	printf("%.17f\n",f);
+	ft_ftoa(f,17);
 	
 	return (0);
 }
-
-// exp -14 -15
-//0.000091552734375
-//0.000091552734375
-
-// exp -17 -18
-//0.000011444091796875
-//0.000011444091796875
-
-// STATUS
-//count fraction tulostaa nyt oikean aina "loppuun saakka eli -126"	
