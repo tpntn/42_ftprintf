6f7e931192d93a4dfbc3576ea6974789b0815de0
Timo Pontinen <toponen@gmail.com>
2022-10-17 11:25:29 +0300

    started working on ints

---
+++ printf_ints.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_ints.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpontine <tpontine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 10:59:54 by tpontine          #+#    #+#             */
+/*   Updated: 2022/10/17 11:25:10 by tpontine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "ft_printf.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+#include "Libft/libft.h"
+
+static int	ft_nbr_len(long long nb)
+{
+	int	i;
+
+	i = 1;
+	if (nb < 0)
+	{
+		i++;
+		nb *= -1;
+	}
+	while (nb > 9)
+	{
+		nb /= 10;
+		i++;
+	}
+	return (i);
+}
+
+static long	ft_div(int len)
+{
+	long	i;
+
+	i = 1;
+	if (len == 1)
+		return (1);
+	while (len > 1)
+	{
+		i *= 10;
+		len--;
+	}
+	return (i);
+}
+
+char	*ft_dtoa(long long n)
+{
+	int		i;
+	int		len;
+	int		len2;
+	char	*result;
+
+	i = 0;
+	len = ft_nbr_len(n);
+	len2 = len;
+	result = (char *)malloc(sizeof(char) * ((size_t)len + 1));
+	if (!result)
+		return (NULL);
+	if (n == -9223372036854775807)
+		return (ft_strdup("-9223372036854775807"));
+	if (n < 0)
+	{
+		n *= -1;
+		result[0] = '-';
+		i++;
+		len--;
+	}
+	while (i < len2)
+		result[i++] = ((n / ft_div(len--)) % 10) + 48;
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_ulltoa(unsigned long long n)
+{
+	int		i;
+	int		len;
+	int		len2;
+	char	*result;
+
+	i = 0;
+	len = ft_nbr_len(n);
+	len2 = len;
+	result = (char *)malloc(sizeof(char) * ((size_t)len + 1));
+	if (!result)
+		return (NULL);
+	while (i < len2)
+		result[i++] = ((n / ft_div(len--)) % 10) + 48;
+	result[i] = '\0';
+	return (result);
+}
+
+int	main()
+{
+	long long l = 9223372036854775807;
+	unsigned long long int longlong = 18446744073709551614;
+	// int len = ft_nbr_len(l);
+	printf("%s\n", ft_dtoa(l));
+	printf("%s\n", ft_ulltoa(longlong));
+	
+	
+	return (0);
+}
